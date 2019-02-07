3cf739f35ea04f14adb6b8d012ff21ebb71cf298
Dylan Wright <sealsurlaw@gmail.com>
2019-02-06 20:25:08 -0800

    Finished functions.js

--- node/functions.js
+++ node/functions.js
@@ -15,5 +15,11 @@ arrow('hi')
 // for practice
 
 // make an arrow function with no block body
+const myArrow = (y) => y + 1;
+console.log(myArrow(2));
 
 // make a function that returns a function
+const funcfunc = () => () => console.log('This is weird');
+const a = funcfunc();
+a();
+funcfunc()();
